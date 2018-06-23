68c6c9279b86e8dc295f15f5424b517b1f0cbc65
yanxp <sysuyanxp@163.com>
2018-06-23 16:15:37 +0800

    add seg results

--- README.md
+++ README.md
@@ -4,6 +4,12 @@ This repo is implementation for PointNet(https://arxiv.org/abs/1612.00593) in py
 
 Based on [pointnet.pytorch](https://github.com/fxia22/pointnet.pytorch)
 
+### Results
+
+the segmatation result
+
+<p align=center><img width="80%" src="utils/show3d.png"/></p>
+
 ### Links
 
 The official TensorFlow implementation from the authors can be found [here](https://github.com/charlesq34/pointnet).
--- train/show_seg.py
+++ train/show_seg.py
@@ -17,7 +17,9 @@ from torch.autograd import Variable
 import torch.nn.functional as F
 import importlib as imp
 import torch.utils.data as Data
+import matplotlib.pyplot as plt
 import sys
+
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 ROOT_DIR = os.path.dirname(BASE_DIR)
 sys.path.append(BASE_DIR)
@@ -25,9 +27,11 @@ sys.path.append(os.path.join(ROOT_DIR,'dataset'))
 sys.path.append(os.path.join(ROOT_DIR,'model'))
 sys.path.append(os.path.join(ROOT_DIR,'utils'))
 import show3d_ball
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--model',type=str,default='',help='model path')
-parser.add_argument('--index',type=index,default='10',help='data index')
+parser.add_argument('--index',type=int,default='10',help='data index')
+parser.add_argument('--num_points',type=int,default=2500,help='input points num')
 opt = parser.parse_args()
 print(opt)
 
@@ -36,12 +40,12 @@ pointnet = imp.import_module('pointnet')
 pointdata = imp.import_module('dataset')
 
 dataset = pointdata.PartDataset(root='../data/shapenetcore_partanno_segmentation_benchmark_v0',npoints=opt.num_points,classification=False,train=False,class_choice=['Chair'])
-print("data index %d/%d" %( opt.index, len(ddatset)))
+print("data index %d/%d" %( opt.index, len(dataset)))
 
 num_classes = dataset.num_seg_classes
 print('num_classes:',num_classes)
 
-points, seg = dataset[index]
+points, seg = dataset[opt.index]
 points_np = points.numpy()
 
 cmap = plt.cm.get_cmap("hsv", 10)
@@ -57,9 +61,8 @@ points = points.view(1, points.size()[0], points.size()[1])
 points = points.cuda()
 pred, _ = model(points)
 index = pred.data.max(2)[1]
-print(index)
 print(index.size())
 pred_color = cmap[index.data.cpu().numpy()[0], :]
 print(pred_color.shape)
-showpoints(point_np, gt, pred_color)
+show3d_ball.showpoints(points_np, gt, pred_color)
 
